2af4a972f2aaad18d8f1fad55e5ad88bf8439f66
Kounex <Kounex@users.noreply.github.com>
2024-05-22 21:27:25 +0000

    deploy: b270361b4a0e7b0bb275a1dbfc886d413b432f02

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1059157394"
+    serviceWorkerVersion: "3188319985"
   }
 });
